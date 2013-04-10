b809d2c1fd9ec28e6e19418a738f4b330827d079
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-04-10 16:28:51 +0200

    Add banner manager

--- kk/bannertool/bannermanager.py
+++ kk/bannertool/bannermanager.py
@@ -21,30 +21,17 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from kk.bannertool import MessageFactory as _
 
 
-# Interface class; used to define content-type schema.
-
-class IBanner(form.Schema, IImageScaleTraversable):
+class IBannerManager(form.Schema, IImageScaleTraversable):
     """
-    A singel banner containing images and captions
+    A folderish collection of banners.
     """
-    image = NamedBlobImage(
-        title=_(u"Banner Image"),
-        required=True,
-    )
-    text = RichText(
-        title=_(u"Formated Banner Text"),
-        description=_(u"Optional banner text with html formatting. If you "
-                      u"leave this field empty the description will be used "
-                      u"instead."),
-        required=False,
-    )
 
 
-class Banner(dexterity.Container):
-    grok.implements(IBanner)
+class BannerManager(dexterity.Container):
+    grok.implements(IBannerManager)
 
 
 class View(grok.View):
-    grok.context(IBanner)
+    grok.context(IBannerManager)
     grok.require('zope2.View')
     grok.name('view')
